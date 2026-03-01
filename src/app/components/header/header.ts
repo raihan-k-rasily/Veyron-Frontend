4a3914de63410604df70a1b10176345bbe265958
raihan-k-rasily <raihankrasily@gmail.com>
2026-03-01 22:44:04 +0530

    Day-6

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from "@angular/router";
+import { Router, RouterLink, RouterModule  } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    
+    // Prevent scrolling when menu is open
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
 
 }
